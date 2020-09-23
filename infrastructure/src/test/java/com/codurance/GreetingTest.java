605b31d2dca4eaa716600778cb2aa3fc21461fce
Tom <thomas.spencer@codurance.com>
2020-09-23 11:55:48 +0100

    Add modules and first test for GreetingController

---
+++ infrastructure/src/test/java/com/codurance/GreetingTest.java
@@ -0,0 +1,26 @@
+package com.codurance;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GreetingTest {
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+
+  @Test
+  void home_should_return_greeting() {
+    assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello", String.class), is("hello"));
+  }
+}
